9a16f6b99124c918aa8dffe2bfaac47b606b502b
Lazar Arsić <lazar.arsic@snapav.com>
2023-05-25 21:28:21 +0200

    Message construction methods further

--- classes/pgp.py
+++ classes/pgp.py
@@ -2,12 +2,14 @@ from classes import user
 from modules import modules
 from modules import constants
 import time
+import zlib
 from Crypto.Hash import SHA1
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import DSA
 from Crypto.Signature import DSS
 from Crypto.PublicKey import ElGamal
+
 
 MessageDict = {
     "filename": None,
@@ -23,14 +25,14 @@ SignatureDict = {
     "signature": None  # for DSA
 }
 
-SessionKeyDict = {
-    "recipient_key_id": None,
-    "session_key_cypher": None
-}
-
 MessageSignatureDict = {
     "signature": SignatureDict.copy(),
     "message": MessageDict.copy()
+}
+
+SessionKeyDict = {
+    "recipient_key_id": None,
+    "session_key_cypher": None
 }
 
 PayloadDict = {
@@ -45,15 +47,21 @@ PGPMessageDict = {
 }
 
 
-def construct_message(filename, message, sign_encrypt_choice, signing_key, encryption_key, encryption_algorythm,
+def construct_message(filename, message, message_time, sign_encrypt_choice, signing_key, encryption_key, session_algorythm,
                       session_key, use_zip, use_radix64):
+
     my_message = MessageDict.copy()
     my_message["filename"] = filename
     my_message["data"] = message
-    my_message["timestamp"] = time.time()
+    my_message["timestamp"] = message_time
+
+    my_pgp = PGPMessageDict.copy()
+    my_pgp["radix64"] = use_radix64
+    my_pgp["zip"] = use_zip
 
     my_signature = SignatureDict.copy()
-    my_signature["sender_key_id"] = signing_key.public_key().export_key()[-8:]
+    #id for key pair of sender
+    my_signature["sender_key_id"] = signing_key.public_key().export_key()[-8:].hex()
     my_signature["leading_octets"]
 
     message_hash = SHA1.new(message).digest()
@@ -65,10 +73,27 @@ def construct_message(filename, message, sign_encrypt_choice, signing_key, encry
         signature = signature_dsa(message_hash, signing_key)
         session_key_component = encrypt_elgamal(encryption_key, session_key)
     else:
-        raise ValueError("Invalid signature+ecnryption choice.")
+        raise ValueError("Invalid signature+encryption choice.")
 
     my_signature["signature"] = signature
     my_signature["timestamp"] = time.time()
+
+    my_message_signature = MessageSignatureDict.copy()
+    my_message_signature["message"] = my_message
+    my_message_signature["signature"] = signature
+
+    my_session_key = SessionKeyDict.copy()
+    #id for key pair for recipient
+    my_session_key["recipient_key_id"] = encryption_key.export()[-8:].hex()
+    my_session_key["session_key_cypher"] = session_key_component
+
+    my_payload = PayloadDict.copy()
+    my_payload["session_key"] = my_session_key
+    if use_zip:
+        payload = my_message_signature
+    else:
+        my_payload = my_message_signature
+    my_payload["encrypted_data"] =
 
 
 def signature_rsa(message_hash, signing_key):
@@ -96,3 +121,8 @@ def encrypt_elgamal(encryption_key, session_key):
     encrypted_session_key = encryption_key.encrypt(session_key, None)
     print("Encrypted Key:", encrypted_session_key.hex())
     return encrypted_session_key
+
+def zip_payload(payload):
+    return zlib.compress(payload.encode())
+def unzip_payload(payload):
+    return zlib.decompress(payload.dencode())
