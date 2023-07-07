0259c2d7453236dfe4a38c1e9726fb669997fc12
Milovn-Krstic <101987565+Milovn-Krstic@users.noreply.github.com>
2023-07-08 00:17:36 +0200

    started redesign of gui

--- main.py
+++ main.py
@@ -1,119 +1,37 @@
-from classes.pgp import *
-from modules.modules import clear_terminal
-from classes.user import User
-from modules import constants
-import time
-import os
-from Crypto.PublicKey import RSA, DSA, ElGamal
+import tkinter as tk
 
+# Create the main Tkinter window
+root = tk.Tk()
+root.title("Custom Title Bar")
+root.geometry("400x300")
+root.configure(bg='#212121')  # Set background color to dark gray
 
-def main():
-    print("=======================__PGP__===========================")
-    print("=========================================================")
-    name = input("Enter your name\n>: ")
-    email = input("Enter your email\n>: ")
-    user = User(name, email)
-    key_password = "GavriloNub"
+# Disable default title bar
+root.overrideredirect(True)
 
-    user.generate_key_pair(1, 1024, key_password)
-    user.generate_key_pair(2, 1024, key_password)
+# Create custom frame for title bar
+frame_title = tk.Frame(root, bg='#2e7d32', height=30)
+frame_title.pack(fill='x')
 
-    testmain(user, key_password)
+# Add widgets to the custom title bar
+label_title = tk.Label(frame_title, text="Custom Title Bar", fg='white', bg='#2e7d32')
+label_title.pack(side='left', padx=10)
 
-    state = 1
-    while state != 0:
-        clear_terminal()
-        print("***User Interface***\n1)see info\n2)generate key pair\n"
-              + "3)export keys\n4)import keys\n5)see all key pairs\n6)send a message\n0)exit")
-        state = int(input(">: "))
-        if state == 1:
-            print(user.get_info())
-        if state == 2:
-            algorithm = int(
-                input("Select algorithm:\n1)RSA encryption/signature\n2)DES signature and ElGamal encryption\n>: "))
-            key_size = int(input("Enter key size (1024 or 2048)\n>: "))
+button_minimize = tk.Button(frame_title, text="-", fg='white', bg='#2e7d32')
+button_minimize.pack(side='right')
 
-            while key_size != 1024 and key_size != 2048:
-                key_size = int(input("Enter key size (1024 or 2048)\n>: "))
+button_maximize = tk.Button(frame_title, text="□", fg='white', bg='#2e7d32')
+button_maximize.pack(side='right')
 
-            key_password = input("Enter password for private key\n>: ")
-            user.generate_key_pair(algorithm, key_size, key_password)
-            print("key pair successfully generated ...")
-        if state == 3:
-            user.export_private_key(path=f'./private/{user.name}/keys/')
-        if state == 4:
-            user.import_private_key(f'./private/{user.name}/keys/file.pem')
-        if state == 5:
-            user.show_key_chain(password_in=input("[ENTER PASSWORD] >:"))
-        if state == 6:
-            pass
+button_close = tk.Button(frame_title, text="X", fg='white', bg='#2e7d32', command=root.destroy)
+button_close.pack(side='right')
 
+# Create content frame
+frame_content = tk.Frame(root, bg='white')
+frame_content.pack(fill='both', expand=True)
 
-if __name__ == '__main__':
-    main()
+# Add content widgets to the content frame
+label_content = tk.Label(frame_content, text="Hello, World!", font=('Arial', 16), padx=20, pady=20)
+label_content.pack()
 
-def testmain(user: User, key_password):
-    pp = pprint.PrettyPrinter(depth=4)
-    key_id_hex_string0 = hex(int.from_bytes(user.private_key_chain[0]["key_id"], byteorder='big'))
-    key_id_hex_string1 = hex(int.from_bytes(user.private_key_chain[1]["key_id"], byteorder='big'))
-    user.export_private_key(f'./private/{user.name}/keys/{user.email}_{key_id_hex_string0}.pem', user.private_key_chain[0]["key_id"])
-    user.import_private_key(f'./private/{user.name}/keys/{user.email}_{key_id_hex_string0}.pem', key_password)
-    user.export_private_key(f'./private/{user.name}/keys/{user.email}_{key_id_hex_string1}.pem', user.private_key_chain[1]["key_id"])
-    user.import_private_key(f'./private/{user.name}/keys/{user.email}_{key_id_hex_string1}.pem', key_password)
-
-    user.export_public_key(f'./public/{user.name}/keys/{user.email}_{key_id_hex_string0}.pem',
-                           user.private_key_chain[0]["key_id"])
-    user.import_public_key(f'./public/{user.name}/keys/{user.email}_{key_id_hex_string0}.pem')
-    user.export_public_key(f'./public/{user.name}/keys/{user.email}_{key_id_hex_string1}.pem',
-                           user.private_key_chain[1]["key_id"])
-    user.import_public_key(f'./public/{user.name}/keys/{user.email}_{key_id_hex_string1}.pem')
-
-    message = b"Neka pristojna poruka za testiranje, ne znam kako cu prevodioce da polozim j*** me fakultet nezavrseni"
-
-    result = construct_message("Poruka", message, time.time(), key_password, user.private_key_chain[1],
-                               user.public_key_chain[1], constants.SIGN_ENC_DSA_ELGAMAL, constants.ALGORITHM_AES,
-                               use_signature=True, use_zip=True,
-                               use_radix64=True)
-    pp.pprint(result)
-    my_pgp, key_id = extract_and_validate_message_1(result)
-    # ask user for password for given key_id if key_id != None
-    extracted = extract_and_validate_message_2(my_pgp, user, key_password)
-    print("---------------")
-    pp.pprint(extracted)
-    print("---------------RED")
-
-    result = construct_message("Poruka", message, time.time(), key_password, user.private_key_chain[1],
-                               user.public_key_chain[1], constants.SIGN_ENC_DSA_ELGAMAL, constants.ALGORITHM_DES3,
-                               use_signature=True, use_zip=True,
-                               use_radix64=True)
-    pp.pprint(result)
-    my_pgp, key_id = extract_and_validate_message_1(result)
-    # ask user for password for given key_id if key_id != None
-    extracted = extract_and_validate_message_2(my_pgp, user, key_password)
-    print("---------------")
-    pp.pprint(extracted)
-    print("---------------RED")
-
-    result = construct_message("Poruka", message, time.time(), key_password, user.private_key_chain[0],
-                               user.public_key_chain[0], constants.SIGN_ENC_RSA, constants.ALGORITHM_AES,
-                               use_signature=True, use_zip=True,
-                               use_radix64=True)
-    pp.pprint(result)
-    my_pgp, key_id = extract_and_validate_message_1(result)
-    # ask user for password for given key_id if key_id != None
-    extracted = extract_and_validate_message_2(my_pgp, user, key_password)
-    print("---------------")
-    pp.pprint(extracted)
-    print("---------------RED")
-
-    result = construct_message("Poruka", message, time.time(), key_password, user.private_key_chain[0],
-                               user.public_key_chain[0], constants.SIGN_ENC_RSA, constants.ALGORITHM_DES3,
-                               use_signature=True, use_zip=True,
-                               use_radix64=True)
-    pp.pprint(result)
-    my_pgp, key_id = extract_and_validate_message_1(result)
-    # ask user for password for given key_id if key_id != None
-    extracted = extract_and_validate_message_2(my_pgp, user, key_password)
-    print("---------------")
-    pp.pprint(extracted)
-    print("---------------RED")
+root.mainloop()
